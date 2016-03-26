96f53dda490e8584a5b2a545a8fa389122f605ea
Franz PC <franzfischer1985@gmail.com>
2016-03-26 12:11:30 +0100

    Delete function installed

---
+++ data/language/messages.de_DE.js
@@ -0,0 +1,14 @@
+/**
+ * Created by root on 26.03.16.
+ */
+var __lang = {
+    Invite: 'Einladen',
+    History: 'History',
+    Save: 'Speichern',
+    Firstname: 'Vorname',
+    Lastname: 'Nachname',
+    Amount: 'Anzahl Personen',
+    Girls: 'Mädels',
+    Invited: 'Eingeladen',
+    Notice: 'Notizen'
+};
